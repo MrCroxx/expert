feb861d1b02a538bd3eed23826ff019bfbcad7e6
Croxx <croxxpwn@gmail.com>
2019-04-30 20:19:27 +0800

    Add Spring Cloud Bus. + Add Config Bus. + Update service-hi config.

--- hi/src/main/kotlin/cn/edu/buaa/se/hi/HiApplication.kt
+++ hi/src/main/kotlin/cn/edu/buaa/se/hi/HiApplication.kt
@@ -2,10 +2,12 @@ package cn.edu.buaa.se.hi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope
 class HiApplication
 
 fun main(args: Array<String>) {
