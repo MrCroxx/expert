e841cc3f556d439502d535add4a31dd1e4114865
Croxx <croxxpwn@gmail.com>
2019-04-30 20:53:54 +0800

    Fix service-hi refresh scope. + Fix service-hi refresh scope. + Update service-hi config.

--- hi/src/main/kotlin/cn/edu/buaa/se/hi/controller.kt
+++ hi/src/main/kotlin/cn/edu/buaa/se/hi/controller.kt
@@ -1,11 +1,13 @@
 package cn.edu.buaa.se.hi
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RefreshScope
 class HiController {
     @Value("\${hi.hi-word}")
     lateinit var hi_word: String
