da51f87d9dd2fb25115ffe37dae85d7bf82247be
Croxx <croxxpwn@gmail.com>
2019-06-02 00:26:23 +0800

    Add Search Doc. + Add Search Doc.

--- docs/src/main/kotlin/cn/edu/buaa/se/docs/controller.kt
+++ docs/src/main/kotlin/cn/edu/buaa/se/docs/controller.kt
@@ -1,5 +1,6 @@
 package cn.edu.buaa.se.docs
 
+import io.swagger.annotations.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.web.bind.annotation.GetMapping
@@ -18,6 +19,21 @@ class SearchController {
     lateinit var searchService: SearchService
 
 
+    /*
+    @ApiOperation(value = "修改专业", notes = "修改专业")
+    @ApiImplicitParam(name = "subject", value = "专业", dataType = "String")
+     */
+    @ApiOperation(value = "查询论文/专利", notes = "查询论文/专利")
+    @ApiResponses(
+            ApiResponse(code = 20000, message = "success")
+    )
+    @ApiImplicitParams(
+            ApiImplicitParam(name = "keyword", value = "搜索关键词", dataType = "String", required = true),
+            ApiImplicitParam(name = "type", value = "搜索类型,论文为'paper',专利为'patent',默认搜索论文", dataType = "String", required = false),
+            ApiImplicitParam(name = "sort", value = "排序方式,时间为'date',点击量为'click',默认按时间排序", dataType = "String", required = false),
+            ApiImplicitParam(name = "year", value = "限定搜索结果年份,默认全部", dataType = "String", required = false),
+            ApiImplicitParam(name = "limit", value = "限定搜索结果最大条目数,默认为12*10条", dataType = "String", required = false)
+    )
     @GetMapping("/")
     fun search(
             keyword: String,
@@ -28,7 +44,7 @@ class SearchController {
     ): CResponseBody<SearchResult?> {
         val eType = DocType.fromString(type ?: "")
         val eSort = SearchSort.fromString(sort ?: "")
-        val nLimit = limit ?: 120
+        val nLimit = limit ?: PAGE_COUNT * 10
         val result = searchService.search(
                 keyword = keyword,
                 type = eType,
